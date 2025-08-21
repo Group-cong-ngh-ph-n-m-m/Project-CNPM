25d31def237e7ba7b844d444dbe6b3a981c8ad88
caominh232006 <minhtc5146@ut.edu.vn>
2025-08-21 23:20:10 +0700

    Update service_model.py

--- service_model.py
+++ service_model.py
@@ -5,6 +5,5 @@ class ServiceEntity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
-
-    # Quan hệ tham khảo (tuỳ cần):
     videos = db.relationship("VideoEntity", backref="service", lazy=True, cascade="all, delete-orphan")
+
