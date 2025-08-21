7b70f1292e5d00f82a6f578f7f214b00c76b2d5c
caominh232006 <minhtc5146@ut.edu.vn>
2025-08-21 23:26:20 +0700

    Delete admin_model.py

--- admin_model.py
+++
@@ -1,9 +0,0 @@
-from infrastructure.databases import db
-
-class AdminEntity(db.Model):
-    __tablename__ = "admins"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False, unique=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=True)  
-
