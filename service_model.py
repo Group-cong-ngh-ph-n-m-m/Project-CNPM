23b8cba59f518010a74e8a0e73b437d664238e39
caominh232006 <minhtc5146@ut.edu.vn>
2025-08-21 23:26:39 +0700

    Delete service_model.py

--- service_model.py
+++
@@ -1,9 +0,0 @@
-from infrastructure.databases import db
-
-class ServiceEntity(db.Model):
-    __tablename__ = "services"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    videos = db.relationship("VideoEntity", backref="service", lazy=True, cascade="all, delete-orphan")
-
