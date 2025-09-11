3698664e2a2613b647fe921d0cf033fd356f37cc
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 22:19:53 +0700

    Update video_model.py

--- video_model.py
+++ video_model.py
@@ -1,9 +1,12 @@
 from infrastructure.databases import db
 
-class VideoEntity(db.Model):
+class VideoModel(db.Model):
     __tablename__ = "videos"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     url = db.Column(db.String(255), nullable=False)
-
+    description = db.Column(db.Text, nullable=True)
     service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
+    def __repr__(self):
+        return f"<VideoModel id={self.id}, title={self.title}, service_id={self.service_id}>"
+
