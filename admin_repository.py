c04f7b67f6acc50bf262ca2484f77c7ae68a5f24
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 21:50:18 +0700

    Update admin_repository.py

--- admin_repository.py
+++ admin_repository.py
@@ -1,32 +1,36 @@
+from typing import List, Optional
+from domain.admin import Admin                
+from infrastructure.models.admin import AdminModel   
 from infrastructure.databases import db
-from infrastructure.models.admin_model import AdminEntity
-
 class AdminRepository:
-    def get_all(self):
-        return AdminEntity.query.all()
-
-    def get_by_id(self, admin_id):
-        return AdminEntity.query.get(admin_id)
-
-    def create(self, data):
-        admin = AdminEntity(**data)
-        db.session.add(admin)
+    def get_all(self) -> List[Admin]:
+        admins = AdminModel.query.all()
+        return [
+            Admin(id=a.id, username=a.username, email=a.email)
+            for a in admins
+        ]
+    def get_by_id(self, admin_id: int) -> Optional[Admin]:
+        a = AdminModel.query.get(admin_id)
+        if not a:
+            return None
+        return Admin(id=a.id, username=a.username, email=a.email)
+    def create(self, data: dict) -> Admin:
+        admin_model = AdminModel(**data)
+        db.session.add(admin_model)
         db.session.commit()
-        return admin
-
-    def update(self, admin_id, data):
-        admin = AdminEntity.query.get(admin_id)
-        if not admin:
+        return Admin(id=admin_model.id, username=admin_model.username, email=admin_model.email)
+    def update(self, admin_id: int, data: dict) -> Optional[Admin]:
+        admin_model = AdminModel.query.get(admin_id)
+        if not admin_model:
             return None
         for key, value in data.items():
-            setattr(admin, key, value)
+            setattr(admin_model, key, value)
         db.session.commit()
-        return admin
-
-    def delete(self, admin_id):
-        admin = AdminEntity.query.get(admin_id)
-        if not admin:
+        return Admin(id=admin_model.id, username=admin_model.username, email=admin_model.email)
+    def delete(self, admin_id: int) -> bool:
+        admin_model = AdminModel.query.get(admin_id)
+        if not admin_model:
             return False
-        db.session.delete(admin)
+        db.session.delete(admin_model)
         db.session.commit()
         return True
