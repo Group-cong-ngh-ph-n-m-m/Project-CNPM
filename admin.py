649daa743b3a47651846703d53c14c5b6713e948
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 21:46:16 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,8 +1,9 @@
 class Admin:
-    def __init__(self, id: int = None, username: str = "", email: str = ""):
+    def __init__(self, id: int = None, username: str = "", email: str = "", password: str = None):
         self.id = id
         self.username = username
         self.email = email
-
+        self.password = password
     def __repr__(self):
-        return f"<Admin id={self.id}, username={self.username}>"
+        return f"<Admin id={self.id}, username={self.username}, email={self.email}>"
+
