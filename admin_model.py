8160ebd3d53e6214cb037d29e78f789ea6195fe3
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 21:47:54 +0700

    Update admin_model.py

--- admin_model.py
+++ admin_model.py
@@ -1,8 +1,11 @@
 from infrastructure.databases import db
-
 class AdminEntity(db.Model):
     __tablename__ = "admins"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=True)
+
+    def __repr__(self):
+        return f"<AdminEntity id={self.id}, username={self.username}, email={self.email}>"
