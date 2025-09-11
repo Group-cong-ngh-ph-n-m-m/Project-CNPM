dc1ea478692beb018ed0cadbbccae123827a2915
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 22:13:14 +0700

    Update video.py

--- video.py
+++ video.py
@@ -1,9 +1,16 @@
 class Video:
-    def __init__(self, id: int = None, title: str = "", url: str = "", description: str = ""):
+    def __init__(
+        self,
+        id: int = None,
+        title: str = "",
+        url: str = "",
+        description: str = "",
+        service_id: int = None
+    ):
         self.id = id
         self.title = title
         self.url = url
         self.description = description
-
+        self.service_id = service_id  # liên kết với Service
     def __repr__(self):
-        return f"<Video id={self.id}, title={self.title}>"
+        return f"<Video id={self.id}, title={self.title}, service_id={self.service_id}>"
