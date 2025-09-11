ef1b0203d7ed52e329c9c619f9245ec9822273e4
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-11 22:21:26 +0700

    Update service_model.py

--- service_model.py
+++ service_model.py
@@ -1,16 +1,16 @@
 from infrastructure.databases import db
 
-class ServiceEntity(db.Model):
+class ServiceModel(db.Model):
     __tablename__ = "services"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False, default=0.0)
     videos = db.relationship(
-        "VideoEntity",
+        "VideoModel",               
         backref="service",
         lazy=True,
         cascade="all, delete-orphan"
     )
     def __repr__(self):
-        return f"<ServiceEntity id={self.id}, name={self.name}, price={self.price}>"
+        return f"<ServiceModel id={self.id}, name={self.name}, price={self.price}>"
