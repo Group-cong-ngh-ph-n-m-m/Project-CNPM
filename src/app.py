62229a95385769ba93f35bb4af34df96dfa6d743
caominh232006 <minhtc5146@ut.edu.vn>
2025-09-12 09:22:09 +0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,12 +5,14 @@ from api.controllers.user_controller import bp as user_bp
 from api.controllers.subject_controllers import bp as subject_bp
 from api.controllers.tutor_profile_controllers import bp as tutor_profile_bp
 from api.controllers.message_controllers import bp as message_bp
-from api.controllers.admin_controllers import bp as admin_bp
+from api.controllers.admin_controller import bp as admin_bp
 from api.controllers.booking_controllers import bp as booking_bp
 from api.controllers.complaint_controllers import bp as complaint_bp
 from api.controllers.conversation_controllers import bp as conversation_bp
 from api.controllers.rating_controllers import bp as rating_bp
 from api.controllers.review_controllers import bp as review_bp
+from api.controllers.service_controller import bp as service_bp
+from api.controllers.video_controller import bp as video_bp
 from api.middleware import middleware
 from api.responses import success_response
 from infrastructure.databases import init_db
@@ -35,7 +37,8 @@ def create_app():
     app.register_blueprint(rating_bp)
     app.register_blueprint(conversation_bp)
     app.register_blueprint(review_bp)
-
+    app.register_blueprint(service_bp)
+    app.register_blueprint(video_bp) 
     # Thêm Swagger UI blueprint
     SWAGGER_URL = '/docs'
     API_URL = '/swagger.json'
@@ -56,7 +59,7 @@ def create_app():
     # Tự động quét tất cả các route đã đăng ký
     with app.test_request_context():
         for rule in app.url_map.iter_rules():
-            if rule.endpoint.startswith(('todo.', 'user.', 'subject.', 'tutor_profile.', 'message.', 'admin.', 'booking.', 'complaint.', 'conversation.', 'rating.', 'review.')):
+            if rule.endpoint.startswith(('todo.', 'user.', 'subject.', 'tutor_profile.', 'message.', 'admin.', 'booking.', 'complaint.', 'conversation.', 'rating.', 'review.', "service.", "video.",)):
                 view_func = app.view_functions[rule.endpoint]
                 print(f"Adding path: {rule.rule} -> {view_func}")
                 spec.path(view=view_func)
